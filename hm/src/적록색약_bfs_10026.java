5811982f4b4c2bc1e1c908404f7966702e720541
HyunminKo <hyunmin.ko4578@gmail.com>
2019-03-20 17:43:11 +0900

    Rename and refactoring

--- hm/src/적록색약_bfs_10026.java
+++ hm/src/적록색약_bfs_10026.java
@@ -2,8 +2,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class 적록색약_10026 {
-    static int N;
+public class 적록색약_bfs_10026 {
+    static int N, numOfNormalArea,numOfAbnormalArea;
     static int[][] map;
     static boolean[][] visited;
     static int[] dx = {-1,0,1,0};
@@ -27,56 +27,47 @@ public class 적록색약_10026 {
             }
         }
         Queue<Point> q = new LinkedList<>();
-        int numOfNormalArea = 0;
-        for(int i = 0 ; i < N; i++){
-            for(int j = 0 ; j < N; j++){
-                if(!visited[i][j]){
-                    q.offer(new Point(i,j));
-                    visited[i][j]= true;
-                    while(!q.isEmpty()){
-                        Point p = q.poll();
-                        for(int k = 0 ; k < 4; k++){
-                            int nx = p.x + dx[k];
-                            int ny = p.y + dy[k];
-                            if(nx < 0 || ny < 0 || nx >= N || ny >= N || map[nx][ny] != map[p.x][p.y]) continue;
-                            if(visited[nx][ny]) continue;
-                            q.offer(new Point(nx,ny));
-                            visited[nx][ny] = true;
-                        }
-                    }
-                    numOfNormalArea++;
-                }
-            }
-        }
-        int numOfAbnormalArea = 0;
+        bfs(q, 0);
         visited = new boolean[N][N];
-        for(int i = 0 ; i < N; i++){
-            for(int j = 0 ; j < N; j++){
-                if(!visited[i][j]){
-                    q.offer(new Point(i,j));
-                    visited[i][j]= true;
-                    while(!q.isEmpty()){
-                        Point p = q.poll();
-                        for(int k = 0 ; k < 4; k++){
-                            int nx = p.x + dx[k];
-                            int ny = p.y + dy[k];
-                            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                            if(map[p.x][p.y] == 3){
-                                if(map[nx][ny] != 3) continue;
-                            } else if(map[p.x][p.y] == 2 ||map[p.x][p.y] == 1){
-                                if(map[nx][ny] == 3) continue;
-                            }
-                            if(visited[nx][ny]) continue;
-                            q.offer(new Point(nx,ny));
-                            visited[nx][ny] = true;
-                        }
-                    }
-                    numOfAbnormalArea++;
-                }
-            }
-        }
+        bfs(q,1);
         System.out.println(numOfNormalArea+" "+numOfAbnormalArea);
     }
+
+    private static void bfs(Queue<Point> q, int normal) { // 0: normal, 1: abnormal
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0 ; j < N; j++){
+                if(!visited[i][j]){
+                    q.offer(new Point(i,j));
+                    visited[i][j]= true;
+                    while(!q.isEmpty()){
+                        Point p = q.poll();
+                        for(int k = 0 ; k < 4; k++){
+                            int nx = p.x + dx[k];
+                            int ny = p.y + dy[k];
+                            if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
+                            if(normal == 0){ // 정상
+                                if(map[nx][ny] != map[p.x][p.y]) continue;
+                            }else { // 비정상
+                                if(map[p.x][p.y] == 3){
+                                    if(map[nx][ny] != 3) continue;
+                                } else if(map[p.x][p.y] == 2 ||map[p.x][p.y] == 1){
+                                    if(map[nx][ny] == 3) continue;
+                                }
+                            }
+                            q.offer(new Point(nx,ny));
+                            visited[nx][ny] = true;
+                        }
+                    }
+                    if(normal == 0){
+                        numOfNormalArea++;
+                    }else {
+                        numOfAbnormalArea++;
+                    }
+                }
+            }
+        }
+    }
+
     static class Point {
         int x,y;
 
