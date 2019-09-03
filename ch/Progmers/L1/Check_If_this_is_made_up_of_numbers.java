0a6c4d3cf96a7b70d7beb6301a31bc2b42188152
Chang <newckdgns@naver.com>
2019-09-03 09:46:07 +0900

    Add programmers L1 code

---
+++ ch/Progmers/L1/Check_If_this_is_made_up_of_numbers.java
@@ -0,0 +1,24 @@
+package level1;
+
+public class Check_If_this_is_made_up_of_numbers {
+    public static void main(String[] args) {
+        boolean answer = solution("01234b6789");
+        System.out.println(answer);
+    }
+
+    public static boolean solution(String s) {
+        boolean answer = true;
+        char[] pieces = s.toCharArray();
+        int l = pieces.length;
+        if (l == 4 || l == 6)
+            for (int i = 0; i < l; i++) {
+                if (48 <= pieces[i] && pieces[i] <= 57)
+                    continue;
+                else
+                    answer = false;
+            }
+        else
+            answer = false;
+        return answer;
+    }
+}
