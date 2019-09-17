9ff55afd33374b0606a9a7a57bbfd69288f276f3
Chang <newckdgns@naver.com>
2019-09-17 11:17:00 +0900

    Upload programmers L3 code

---
+++ ch/Progmers/L3/Network.java
@@ -0,0 +1,30 @@
+public class Network {
+    static boolean[][] link;
+
+    public static void main(String[] args) {
+        int a = solution(3, new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}});
+        System.out.println(a);
+    }
+
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+        link = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            if (!link[i][i]) {
+                dfs(computers, i, n);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void dfs(int[][] computers, int idx, int n) {
+        for (int i = 0; i < n; i++) {
+            if (computers[idx][i] == 1 && !link[idx][i]) {
+                link[idx][i] = link[i][idx] = true;
+                dfs(computers, i, n);
+            }
+        }
+    }
+}
